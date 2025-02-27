af5913b072ba3a356744eb9919aec117a0298eab
AceSumaoy <sumaoyace78@gmail.com>
2025-02-27 13:36:58 +0800

    Updated ModelForm and EmployeeDetail

--- client/src/pages/Form/ModalForm.tsx
+++ client/src/pages/Form/ModalForm.tsx
@@ -7,8 +7,72 @@ type ModalFormProps = {
   register: UseFormRegister<PDSForm>;
 };
 
+const positions = [
+  "Accounting Processor B",
+  "Administrative Services Aide",
+  "Administrative Services Officer A",
+  "Administration Services Assistant A",
+  "Administration Services Assistant C",
+  "Cashier D",
+  "Collection Assistant",
+  "Customer Service Assistant A",
+  "Customer Service Assistant B",
+  "Customer Service Assistant C",
+  "Customer Service Assistant D",
+  "Customer Service Officer B",
+  "Division Manager C",
+  "Driver",
+  "Engineering Aide A",
+  "Engineering Aide B",
+  "Engineering Assistant A",
+  "General Manager",
+  "Senior Accounting Processor B",
+  "Senior Corporate Accountant C",
+  "Senior Engineer A",
+  "Storekeeper B",
+  "Supply Assistant B",
+  "Utility Worker A",
+  "Utility Worker B",
+  "Utilities Service Assistant E",
+  "Water Maintenance Foreman",
+  "Water Maintenance Man A",
+  "Water Maintenance Man B",
+  "Water Maintenance Man C",
+  "Water Resources Facilities Operator A",
+  "Water Resources Facilities Operator B",
+  "Water Resources Facilities Operator C",
+  "Water Resources Facilities Operator Foreman",
+  "Water Resources Facilities Tender A",
+  "Water Resources Facilities Tender B",
+  "Water Utilities Development Officer A",
+];
+
 const ModalForm = ({ register }: ModalFormProps) => {
   const [showModal, setShowModal] = useState(true);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredPositions, setFilteredPositions] = useState<string[]>([]);
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setSearchTerm(value);
+
+    if (value === "") {
+      setFilteredPositions([]);
+      setShowDropdown(false);
+    } else {
+      const filtered = positions.filter((position) =>
+        position.toLowerCase().includes(value.toLowerCase()),
+      );
+      setFilteredPositions(filtered);
+      setShowDropdown(true);
+    }
+  };
+
+  const handleSelect = (position: string) => {
+    setSearchTerm(position);
+    setShowDropdown(false);
+  };
 
   return (
     showModal && (
@@ -20,22 +84,29 @@ const ModalForm = ({ register }: ModalFormProps) => {
           }}
           className="w-96 rounded-lg bg-white p-6 shadow-lg"
         >
-          <h2 className="mb-4 text-xl font-bold">
-            Employment Type and Department
+          <h2 className="mb-4 text-center text-xl font-bold">
+            Employee Information
           </h2>
           <div className="mb-4">
             <label htmlFor="employee_type" className="block font-semibold">
-              Employment Type:
+              ID No.:
+            </label>
+            <input type="number" className="mt-1 w-full rounded border p-2" />
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="employee_type" className="block font-semibold">
+              Employment Status:
             </label>
             <select
               id="employee_type"
               defaultValue=""
               {...register("position")}
               required
-              className="mt-1 w-full border p-2"
+              className="mt-1 w-full rounded border p-2"
             >
               <option value="" disabled>
-                Select an employment type
+                Select an employment status
               </option>
               <option value="PERMANENT">Permanent</option>
               <option value="CASUAL">Casual</option>
@@ -45,6 +116,30 @@ const ModalForm = ({ register }: ModalFormProps) => {
               <option value="TEMPORARY">Temporary</option>
             </select>
           </div>
+
+          <div className="relative w-full">
+            <input
+              type="text"
+              value={searchTerm}
+              onChange={handleChange}
+              placeholder="Enter Employee Position"
+              className="w-full rounded border p-2"
+            />
+            {showDropdown && filteredPositions.length > 0 && (
+              <ul className="absolute mt-1 max-h-40 w-full overflow-y-auto rounded border bg-white shadow-lg">
+                {filteredPositions.map((position, index) => (
+                  <li
+                    key={index}
+                    onClick={() => handleSelect(position)}
+                    className="cursor-pointer p-2 hover:bg-gray-200"
+                  >
+                    {position}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+
           <div className="mb-4">
             <label htmlFor="department" className="block font-semibold">
               Department:
@@ -54,7 +149,7 @@ const ModalForm = ({ register }: ModalFormProps) => {
               defaultValue=""
               {...register("department")}
               required
-              className="mt-1 w-full border p-2"
+              className="mt-1 w-full rounded border p-2"
             >
               <option value="" disabled>
                 Select a department
@@ -66,6 +161,13 @@ const ModalForm = ({ register }: ModalFormProps) => {
               <option>Finance and Commercial Division</option>
               <option>Office of the General Manager</option>
             </select>
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="department" className="block font-semibold">
+              First Day of Service:
+            </label>
+            <input type="date" className="mt-1 w-full rounded border p-2" />
           </div>
           <div className="flex justify-center">
             <button
