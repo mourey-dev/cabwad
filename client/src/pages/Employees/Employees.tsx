e50918d0551efadcaeca6eb0548cbad89de8f5f6
AceSumaoy <sumaoyace78@gmail.com>
2025-03-06 09:06:56 +0800

    Added responsiveness to employee.tsx

--- client/src/pages/Employees/Employees.tsx
+++ client/src/pages/Employees/Employees.tsx
@@ -59,14 +59,14 @@ const Employees = () => {
             <option value="TEMPORARY">TEMPORARY</option>
           </select>
         </div>
-        <div className="mb-23 grid grid-cols-5 gap-6 px-6 py-6">
+        <div className="mb-6 grid grid-cols-1 gap-6 px-4 py-6 sm:grid-cols-2 sm:px-6 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
           {data?.map((item) => (
             <div
               key={item.employee_id}
               className="cursor-pointer"
               onClick={() => handleOpenModal(item)}
             >
-              <div className="relative flex h-50 flex-col items-center rounded-md bg-white p-4 shadow-md transition-all delay-150 duration-300 hover:bg-blue-600">
+              <div className="relative flex w-full flex-col items-center rounded-md bg-white p-4 shadow-md transition-transform duration-300 hover:scale-105 hover:bg-blue-600 sm:p-6">
                 <button className="absolute top-2 right-2">
                   <img src={remove} alt="Remove User" className="w-6" />
                 </button>
@@ -75,8 +75,8 @@ const Employees = () => {
                   alt="Employee Icon"
                   className="mt-4 w-16"
                 />
-                <p className="mt-2 font-bold text-gray-800">{`${item.first_name} ${item.surname}`}</p>
-                <p className="text-sm text-gray-500">{`${item.position}`}</p>
+                <p className="mt-2 text-center font-bold text-gray-800">{`${item.first_name} ${item.surname}`}</p>
+                <p className="text-center text-sm text-gray-500">{`${item.position}`}</p>
                 <p className="text-center text-xs text-gray-400">{`${item.department}`}</p>
               </div>
             </div>
